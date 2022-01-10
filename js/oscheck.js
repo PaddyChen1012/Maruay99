4e5e5509f387d54e8cdb268ba28e58c22eff3ac0
DESKTOP-K767DRA\Paddy <paddy_chen@atarroba.com>
2022-01-10 13:32:53 +0800

    測試

--- js/oscheck.js
+++ js/oscheck.js
@@ -1,8 +1,8 @@
 let u = navigator.userAgent;
 let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android終端
 let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios終端
-let dAndroid = document.querySelector('#download_android')
-let diOS = document.querySelector('#download_ios')
+let dAndroid = document.querySelectorAll('[name=download_android]')
+let diOS = document.querySelectorAll('[name=download_ios]')
 function oscheck(){
     if (isAndroid == true && isiOS == false){
         diOS.style.display = 'none';
@@ -13,3 +13,4 @@ function oscheck(){
     }
 }
 oscheck()
+console.log(dAndroid)
