f66eaeae873e177884fee1e2aac1a7363526f54e
DESKTOP-K767DRA\Paddy <paddy_chen@atarroba.com>
2022-02-16 14:05:39 +0800

    加GA 修正活動頁高度 下載按鈕調整

--- js/oscheck.js
+++ js/oscheck.js
@@ -1,18 +1,21 @@
 let u = navigator.userAgent;
 let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android終端
 let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios終端
-let dAndroid = document.querySelectorAll('[name=download_android]')
-let diOS = document.querySelectorAll('[name=download_ios]')
+let dAndroid = document.querySelector('#download_android')
+let diOS = document.querySelector('#download_ios')
+let dlbtn = document.querySelector('#download_btn')
 
 function oscheck(){
-    if (isAndroid == true && isiOS == false){
-        diOS[0].style.display = 'none';
-        diOS[2].style.display = 'none';
-    }else if (isAndroid == false && isiOS == true){
-        dAndroid[0].style.display = 'none';
-        dAndroid[2].style.display = 'none';
+    if (isAndroid == false && isiOS == true){
+        dlbtn.href = 'https://web.maruay99.com/common/receive/Counter/Maruay_Mobil_QRCodeLink.aspx?gid=MARUAY99';
+        if (dAndroid){
+            dAndroid.style.display = 'none';
+        }
     }else {
-        return
+        dlbtn.href = 'https://play.google.com/store/apps/details?id=com.starcube.Maruay99&shortlink=WebMR99Download&pid=Web-MR99-Download';
+        if (diOS){
+            diOS.style.display = 'none';
+        }
     }
 }
 oscheck()
