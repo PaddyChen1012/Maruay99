c4e50158733906dcd1f65c0b8ae6de5df3221caa
DESKTOP-K767DRA\Paddy <paddy_chen@atarroba.com>
2022-02-21 15:13:01 +0800

    下載按鈕 / 連結修正

--- js/oscheck.js
+++ js/oscheck.js
@@ -1,21 +1,13 @@
 let u = navigator.userAgent;
 let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android終端
 let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios終端
-let dAndroid = document.querySelector('#download_android')
-let diOS = document.querySelector('#download_ios')
 let dlbtn = document.querySelector('#download_btn')
 
 function oscheck(){
     if (isAndroid == false && isiOS == true){
-        dlbtn.href = 'https://web.maruay99.com/common/receive/Counter/Maruay_Mobil_QRCodeLink.aspx?gid=MARUAY99';
-        if (dAndroid){
-            dAndroid.style.display = 'none';
-        }
+        dlbtn.href = 'https://storage.googleapis.com/maruay/apk/maruay99.apk';
     }else {
         dlbtn.href = 'https://play.google.com/store/apps/details?id=com.starcube.Maruay99&shortlink=WebMR99Download&pid=Web-MR99-Download';
-        if (diOS){
-            diOS.style.display = 'none';
-        }
     }
 }
 oscheck()
