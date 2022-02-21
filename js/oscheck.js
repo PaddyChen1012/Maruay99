4cf7ab8765419dbf62ddc42dfbaac3c18ff2032e
DESKTOP-K767DRA\Paddy <paddy_chen@atarroba.com>
2022-02-21 15:58:30 +0800

    換連結

--- js/oscheck.js
+++ js/oscheck.js
@@ -5,7 +5,7 @@ let dlbtn = document.querySelector('#download_btn')
 
 function oscheck(){
     if (isAndroid == false && isiOS == true){
-        dlbtn.href = 'https://storage.googleapis.com/maruay/apk/maruay99.apk';
+        dlbtn.href = 'https://cdn.maruay99.com/';
     }else {
         dlbtn.href = 'https://play.google.com/store/apps/details?id=com.starcube.Maruay99&shortlink=WebMR99Download&pid=Web-MR99-Download';
     }
